f55b24bed32522fc07c051de161f36fe3a3a9ed2
IgorPrivalikhin <iprivalihin@mail.ru>
2023-01-10 15:31:18 +0300

    Initial Commit

---
+++ TrafficLightsSwiftUI/TrafficLightsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsSwiftUIApp.swift
+//  TrafficLightsSwiftUI
+//
+//  Created by Анастасия Булдакова on 10.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
